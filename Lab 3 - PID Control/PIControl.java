2377c6030e92f07842de40541621fcca6de7375f
Gabriel Arpino <gabrielarpino@users.noreply.github.com>
2016-11-24 17:19:07 -0500

    updated derivative term

--- Lab 3 - PID Control/PIControl.java
+++ Lab 3 - PID Control/PIControl.java
@@ -1,4 +1,3 @@
-import lejos.hardware.Button;
 import lejos.hardware.lcd.*;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
@@ -9,53 +8,71 @@ import lejos.hardware.motor.*;
 // MOTOR C: RIGHT WHEEL
 // MOTOR B: LEFT WHEEL
 
-public class PIControl{
-	public static void main(String[] args) throws Exception { 
+public class PIDControl
+{
+	public static void main(String[] args) throws Exception 
+	{ 
 		EV3ColorSensor color = new EV3ColorSensor(SensorPort.S3);
 		LCD.clear();
 		double correction;
 		double integral = 0;
+		double derivative;
+		double lasterror = 0;
+		double desired = 0.25;
+		double Kp = 20000;
+		double Ki = 250;
+		double Kd = 100;
+		int speed = 50;
+		int angle = 10;
+
 		while (!Button.ENTER.isDown()) 
 		{
 			int sampleSize = color.sampleSize();
 			float[] redsample = new float[sampleSize];
 			color.getRedMode().fetchSample(redsample, 0); //not sure if RedMode matters
 			//added code
-			double desired = 0.25;
-			double Kp = 20000;
-			double Ki = 100;
-			
 			double actual = redsample[0]; // not sure if sampleSize is the colour sensor value
 			double error = desired - actual;
 			integral = integral + error;
-			correction = error*Kp + Ki*integral; //negative
+			derivative = error - lasterror;
+			if (Button.UP.isDown())
+			{
+				integral = 0;
+			}
+			if (Button.DOWN.isDown())
+			{
+				Kp = 0;
+			}
+			if (Button.RIGHT.isDown())
+			{
+				Kp = Kp + 50;
+			}
+			if (Button.LEFT.isDown())
+			{
+				Kp = Kp - 50;
+			}
+			correction = Kp*error + Ki*integral+Kd*derivative; //negative
 			turn(correction);
 			//end added code
 			LCD.clear();
 			//System.out.println(redsample[0]);
-			System.out.println(correction);
+			System.out.println(correction, speed, angle);
+			System.out.println("Kp");
+			System.out.println(Kp);
+			lasterror = error;
+			if (error*lasterror < 0)
+			{
+				integral = 0;
+			}
 		}
 		color.close();
 	}
 
-	public static void turn(double direction) throws Exception {		
-		if (direction > 0) {
-			int speed = (int) direction;
-			Motor.B.setSpeed(0);
-			Motor.C.setSpeed(speed); // one wheel turns faster
-		}
-		else if (direction < 0) {
-			int speed = (int) direction*(-1);			
-			Motor.B.setSpeed(speed);
-			Motor.C.setSpeed(0);
-		}
-		else {
-			Motor.B.setSpeed(70);
-			Motor.C.setSpeed(70);
-		}
-		Motor.B.rotate(5,true);
-		Motor.C.rotate(5,true);
-			Motor.B.setSpeed(10);
-			Motor.C.setSpeed(10);
+	public static void turn(double direction, int speed, int angle) throws Exception 
+	{		
+		Motor.B.setSpeed(speed-direction);
+		Motor.C.setSpeed(speed+direction);
+		Motor.B.rotate(angle,true);
+		Motor.C.rotate(angle,true);
 	}
 }
